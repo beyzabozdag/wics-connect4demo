cc2d51ccb58461ee8a15802c27b5c7f655e3dea4
beyzabozdag <nbeyzabozdag@gmail.com>
2023-11-08 15:18:25 -0700

    first commit

---
+++ connect4.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pygame
+import sys
+import math
+
+ROW_COUNT = 6  # number of rows in the board
+COLUMN_COUNT = 7 # number of columns in the board
+
+def create_board():
+	"""
+	Creates a 6x7 board
+    """
+	# ADD CODE HERE
+	pass
+
+def drop_piece(board, row, col, piece):
+	"""
+	Places a piece at the specified location
+	"""
+	# ADD CODE HERE
+	pass
+
+def is_valid_location(board, col):
+	"""
+    Checks if the top row of the column is empty
+    """
+    # ADD CODE HERE
+	pass
+
+def get_next_open_row(board, col):
+	"""
+    Returns the next open row in the column
+    """
+    # ADD CODE HERE
+	pass
+
+def print_board(board):
+	"""
+	Prints the board
+	"""
+	# ADD CODE HERE
+	pass
+
+def winning_move(board, piece):
+	"""
+	Checks if the player has won
+	"""
+	# ADD CODE HERE
+	pass
+
+
+def draw_board(board):
+	"""
+	Draws the board
+	
+	!This is needed for the graphics!
+    """
+    # ADD CODE HERE
+	pass
+
+
+# ADD THE GAME LOOP HERE
+
