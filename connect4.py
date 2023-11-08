8253a816f73af296e1a0786c456ae02b76c63a6a
beyzabozdag <nbeyzabozdag@gmail.com>
2023-11-08 15:26:52 -0700

    indent issue

--- connect4.py
+++ connect4.py
@@ -7,56 +7,56 @@ ROW_COUNT = 6  # number of rows in the board
 COLUMN_COUNT = 7 # number of columns in the board
 
 def create_board():
-	"""
-	Creates a 6x7 board
-	"""
-	# ADD CODE HERE
-	pass
+    """
+    Creates a 6x7 board
+    """
+    # ADD CODE HERE
+    pass
 
 def drop_piece(board, row, col, piece):
-	"""
-	Drops a piece in the board
-	"""
-	# ADD CODE HERE
-	pass
+    """
+    Drops a piece in the board
+    """
+    # ADD CODE HERE
+    pass
 
 def is_valid_location(board, col):
-	"""
+    """
     Checks if the top row of the column is empty
     """
     # ADD CODE HERE
-	pass
+    pass
 
 def get_next_open_row(board, col):
-	"""
+    """
     Returns the next open row in the column
     """
     # ADD CODE HERE
-	pass
+    pass
 
 def print_board(board):
-	"""
-	Prints the board
-	"""
-	# ADD CODE HERE
-	pass
+    """
+    Prints the board
+    """
+    # ADD CODE HERE
+    pass
 
 def winning_move(board, piece):
-	"""
-	Checks if the player has won
-	"""
-	# ADD CODE HERE
-	pass
+    """
+    Checks if the player has won
+    """
+    # ADD CODE HERE
+    pass
 
 
 def draw_board(board):
-	"""
-	Draws the board
-	
-	!This is needed for the graphics!
+    """
+    Draws the board
+    
+    !This is needed for the graphics!
     """
     # ADD CODE HERE
-	pass
+    pass
 
 
 # ADD THE GAME LOOP HERE
